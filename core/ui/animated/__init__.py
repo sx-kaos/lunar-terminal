e4b18f8f91a226fef3fdaa2f34404464213cef16
kaos <79645506+sx-kaos@users.noreply.github.com>
2025-08-28 13:24:10 +0100

    Delete core directory
    Delete to re-upload with lots of changes.
    Git CLI playing up 🤷‍♂️

--- core/ui/animated/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# core/ui/animated/__init__.py
-from __future__ import annotations
-from .progress import ProgressBar
-from .spinner import Spinner
-
-__all__ = ["ProgressBar", "Spinner"]
